ea08b44d9fdde20ce3a4cd737f839bd846e80586
Vojtěch Křižan <voytxt@voytxt.com>
2025-12-30 23:59:41 +0100

    default settings

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ export default class FolderNotesPlugin extends Plugin {
 
     // load & save settings
     this.settings = await this.loadData();
+    if (this.settings === null) this.settings = { storageLocation: 'parentFolder' };
     await this.saveData(this.settings);
 
     overrideRevealInFolder(this);
